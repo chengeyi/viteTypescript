a6c87439be1b5a2e01e3b4235baf2773982211d7
chenngeyi <perry564408020@gmail.com>
2024-02-15 20:33:05 +0800

    [2024/02/15]

--- viteScript/src/App.vue
+++ viteScript/src/App.vue
@@ -49,7 +49,7 @@ const arr3 = ref<Person[]>([
 const bool = ref<boolean>(true);
 
 const obj = ref<object>({
-  name:'ton',
+  name: 'ton',
   age: 10
 })
 console.log(obj.value)
@@ -92,11 +92,11 @@ counter.value += 1;
 
 
 onMounted(() => {
-  let bb:string = '調整';
-  let aa:string = '哈哈哈哈開發';
-  let cc:string = '開發2';
-  let dd:string = '開發3';
-  let ee:string = '調整2';
+  let bb: string = '調整';
+  let aa: string = '哈哈哈哈開發';
+  let cc: string = '開發2';
+  let dd: string = '開發3';
+  let ee: string = '調整2';
   class Person {
     //實例屬性
     name: string = '孫悟空';
@@ -118,22 +118,22 @@ onMounted(() => {
   console.log(Person.age)
   Person.sayHello();
 
-  let a:number;
+  let a: number;
   a = 10;
   // a = '1'
   console.log(a)
 
-  let c:boolean = true;
+  let c: boolean = true;
   console.log(c)
 
-  function sum(a:number, b:number):number {
+  function sum(a: number, b: number): number {
     return a + b;
   }
-  console.log(sum(1,2))
+  console.log(sum(1, 2))
 
-  class Dog{
-    name:string;
-    age:number;
+  class Dog {
+    name: string;
+    age: number;
 
     //被稱為構造函數
     //構造函數會在對象創建時調用
@@ -156,6 +156,54 @@ onMounted(() => {
   const dog3 = new Dog('3', 5);
   console.log(dog3)
   dog3.bark();
+
+  const arr = [5, 9, 1, 3, 2, 6];
+  arr.sort(function (a, b) {
+    console.log(a,b)
+    return a - b; // a - b > 0
+  });
+
+
+  //繼承寫法
+  class Animal{
+    name: string;
+    age: number;
+
+    constructor(name:string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+
+    sayHello() {
+      console.log('動物在叫～');
+    }
+  }
+
+  //定義一個表示狗的類
+  //extends表示繼承
+  //狗的類繼承Animal的類
+  //此時Animal被稱為父類，Dogg為子類，繼承後，子類將擁有父類的所有方法和屬性
+  //若子類中添加了與父類相同的方法，則子類方法會覆蓋掉父類的方法，這種形式稱為「方法的重寫」
+  class Dogg extends Animal {
+    run() {
+      console.log(`${this.name}在跑～～～～～～`)
+    }
+
+    sayHello() {
+      console.log('旺旺旺！');
+    }
+  }
+
+  //定義一個表示貓的類
+  //extends表示繼承
+  class cat extends Animal {
+
+  }
+
+  const dogg = new Dogg('旺財', 5);
+  console.log(dogg)
+  dogg.sayHello();
+  dogg.run();
 })
 </script>
 
